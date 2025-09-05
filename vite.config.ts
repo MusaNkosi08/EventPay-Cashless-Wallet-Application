30fc163bf5bdfaa0be4dec73928835b550fd1b83
Musa Banathi Nkosi <bmnkosi08@gmail.com>
2025-09-05 10:37:45 +0200

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+      base: '/EventPay/',  // 👈 add this line
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
